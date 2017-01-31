de5a2cb4fcab7151463c7086ef6d40d7232d5c53
roberto santelices <robertosante@gmail.com>
2017-01-31 13:41:42 -0700

    Added webpack

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,17 +1,8 @@
-/* global React ReactDOM */
+var React = require('react');
+var ReactDOM = require('react-dom');
 
 var div = React.DOM.div;
-var h1 = React.DOM.h1;
-
-var MyTitle = React.createClass({
-  render: function render () {
-    return (
-      div(null,
-        h1({ style: {color: this.props.color} }, this.props.title)
-      )
-    );
-  }
-});
+var MyTitle = require('./MyTitle');
 
 var MyTitleFactory = React.createFactory(MyTitle);
 var ce = React.createElement;
