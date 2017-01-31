67595e83d75fd9b1b71893d387cbee26c1fd47c0
roberto santelices <robertosante@gmail.com>
2017-01-31 12:45:38 -0700

    added Script for client

---
+++ js/ClientApp.js
@@ -0,0 +1,25 @@
+var div = React.DOM.div;
+var h1 = React.DOM.h1;
+
+var MyTitle = React.createClass({
+	render: function render() {
+		return (
+			div(null,
+				h1({style:{color: this.props.color}}, this.props.title)
+			)
+		)
+	}
+});
+
+var MyTitleFactory = React.createFactory(MyTitle);
+var ce = React.createElement;
+
+var MyComponent = (
+	div(null,
+       MyTitleFactory({title: 'Props are great', color: 'blue'}),
+       React.createElement(MyTitle, {title: 'Use props everywhere', color: 'maroon'}),
+       ce(MyTitle, {title: 'again props!', color: 'peru'})
+    )
+);
+    
+ReactDOM.render(MyComponent, document.getElementById('app'));
