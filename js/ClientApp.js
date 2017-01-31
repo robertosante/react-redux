14ea7e1927717109feb70bacf5524d92b7a7cc80
roberto santelices <robertosante@gmail.com>
2017-01-31 13:04:33 -0700

    Added standardjs

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,25 +1,27 @@
-var div = React.DOM.div;
-var h1 = React.DOM.h1;
+/* global React ReactDOM */
+
+var div = React.DOM.div
+var h1 = React.DOM.h1
 
 var MyTitle = React.createClass({
-	render: function render() {
-		return (
-			div(null,
-				h1({style:{color: this.props.color}}, this.props.title)
-			)
-		)
-	}
-});
+  render: function render () {
+    return (
+      div(null,
+        h1({ style: {color: this.props.color} }, this.props.title)
+      )
+    )
+  }
+})
 
-var MyTitleFactory = React.createFactory(MyTitle);
-var ce = React.createElement;
+var MyTitleFactory = React.createFactory(MyTitle)
+var ce = React.createElement
 
 var MyComponent = (
-	div(null,
+    div(null,
        MyTitleFactory({title: 'Props are great', color: 'blue'}),
        React.createElement(MyTitle, {title: 'Use props everywhere', color: 'maroon'}),
        ce(MyTitle, {title: 'again props!', color: 'peru'})
     )
-);
-    
-ReactDOM.render(MyComponent, document.getElementById('app'));
+)
+
+ReactDOM.render(MyComponent, document.getElementById('app'))
